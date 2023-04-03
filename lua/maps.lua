fced4e6c90e32da334b6d38d4704b80bdd0a1de9
David Jonas <djonas@noip.com>
2023-04-03 16:25:04 -0700

    maps: make quickfix easier

--- lua/maps.lua
+++ lua/maps.lua
@@ -12,9 +12,11 @@ map('n', '<Leader>i', ':e ~/.config/nvim<CR>', options)
 map('n', '<Leader>x', ':%s/\\s\\+$//g<CR>', options)
 map('n', '<Leader>b', ':b#<CR>', options)
 map('n', '<Leader>z', '1z=', options)
-map('n', '<Leader>n', ':lne<CR>', options)
-map('n', '<Leader>p', ':set invpaste<CR>', options)
+--map('n', '<Leader>p', ':set invpaste<CR>', options)
 map('n', '<Leader>g', ':Telescope live_grep<CR>', options)
+map('n', '<Leader>c', ':copen<CR>', options)
+map('n', '<Leader>n', ':cne<CR>', options)
+map('n', '<Leader>p', ':cprev<CR>', options)
 
 -- uh, do I use these?
 map('n', '<Leader>f', 'gqis', options)
